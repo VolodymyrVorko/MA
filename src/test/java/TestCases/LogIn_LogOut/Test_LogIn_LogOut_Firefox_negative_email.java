4240c4967c97194037d80fa5c07200ceb3f01d65
VorkoVolodymyr <vorko.volodymyr@gmail.com>
2018-04-24 10:23:12 +0300

    Started adding negative tests

--- src/test/java/TestCases/LogIn_LogOut/Test_LogIn_LogOut_Firefox_negative_email.java
+++ src/test/java/TestCases/LogIn_LogOut/Test_LogIn_LogOut_Firefox_negative_email.java
@@ -1,52 +1,52 @@
-package TestCases.LogIn_LogOut;
-
-import Utility.Constant;
-import Utility.ExcelUtils;
-import Utility.Log;
-import Utility.Utils;
-import appModules.LogInAction;
-import appModules.LogOutAction;
-import org.apache.log4j.xml.DOMConfigurator;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-import pageObjects.BaseClass;
-
-public class Test_LogIn_LogOut_Firefox_negative_email {
-    public WebDriver driver;
-    private String sTestCaseName;
-    private int iTestCaseRow;
-
-    @BeforeMethod
-    public void beforeMethod() throws Exception {
-        DOMConfigurator.configure("log4j.xml");
-        sTestCaseName = this.toString();
-        sTestCaseName = Utils.getTestCaseName(sTestCaseName);
-        Log.startTestCase(sTestCaseName);
-        ExcelUtils.setExcelFile(Constant.Path_TestData + Constant.File_TestData, "Sheet1");
-        iTestCaseRow = ExcelUtils.getRowContains(sTestCaseName, Constant.Col_TestCaseName);
-        driver = Utils.OpenBrowser(iTestCaseRow);
-        new BaseClass(driver);
-    }
-
-    @Test
-    public void Test_LogIn_LogOut_Firefox_negative_email() throws Exception {
-        try {
-            LogInAction.Execute(4);
-            LogOutAction.Execute();
-            ExcelUtils.setCellData("Pass", iTestCaseRow, Constant.Col_Result);
-        } catch (Exception e) {
-            ExcelUtils.setCellData("Fail", iTestCaseRow, Constant.Col_Result);
-            Utils.takeScreenshot(driver, sTestCaseName);
-            Log.error(e.getMessage());
-            throw (e);
-        }
-    }
-
-    @AfterMethod
-    public void afterMethod() {
-        Log.endTestCase(sTestCaseName);
-        driver.close();
-    }
-}
+//package TestCases.LogIn_LogOut;
+//
+//import Utility.Constant;
+//import Utility.ExcelUtils;
+//import Utility.Log;
+//import Utility.Utils;
+//import appModules.LogInAction;
+//import appModules.LogOutAction;
+//import org.apache.log4j.xml.DOMConfigurator;
+//import org.openqa.selenium.WebDriver;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.Test;
+//import pageObjects.BaseClass;
+//
+//public class Test_LogIn_LogOut_Firefox_negative_email {
+//    public WebDriver driver;
+//    private String sTestCaseName;
+//    private int iTestCaseRow;
+//
+//    @BeforeMethod
+//    public void beforeMethod() throws Exception {
+//        DOMConfigurator.configure("log4j.xml");
+//        sTestCaseName = this.toString();
+//        sTestCaseName = Utils.getTestCaseName(sTestCaseName);
+//        Log.startTestCase(sTestCaseName);
+//        ExcelUtils.setExcelFile(Constant.Path_TestData + Constant.File_TestData, "Sheet1");
+//        iTestCaseRow = ExcelUtils.getRowContains(sTestCaseName, Constant.Col_TestCaseName);
+//        driver = Utils.OpenBrowser(iTestCaseRow);
+//        new BaseClass(driver);
+//    }
+//
+//    @Test
+//    public void Test_LogIn_LogOut_Firefox_negative_email() throws Exception {
+//        try {
+//            LogInAction.Execute(4);
+//            LogOutAction.Execute();
+//            ExcelUtils.setCellData("Pass", iTestCaseRow, Constant.Col_Result);
+//        } catch (Exception e) {
+//            ExcelUtils.setCellData("Fail", iTestCaseRow, Constant.Col_Result);
+//            Utils.takeScreenshot(driver, sTestCaseName);
+//            Log.error(e.getMessage());
+//            throw (e);
+//        }
+//    }
+//
+//    @AfterMethod
+//    public void afterMethod() {
+//        Log.endTestCase(sTestCaseName);
+//        driver.close();
+//    }
+//}
